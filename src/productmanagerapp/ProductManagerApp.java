91f4e96d804bf7b46fb54284d6f93c20139aaee9
Zaki Asmat <ngothir@sheridancollege.ca>
2026-02-09 17:06:12 -0500

    Initial Commit By QuynhNgo

---
+++ src/productmanagerapp/ProductManagerApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package productmanagerapp;
+
+/**
+ *
+ * @author trang
+ */
+public class ProductManagerApp {
+
+public static void main(String[] args) {
+        System.out.println("Hello Product Manager");
+    }
+    
+}
